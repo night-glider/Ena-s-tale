38f264cbed5b24d1c3b228c1d5a9f9e09383ed05
night-glider <nightgliderdev@gmail.com>
2022-03-28 19:05:04 +0300

    minor impovements

--- locations/main.gd
+++ locations/main.gd
@@ -5,8 +5,10 @@ extends Spatial
 func _on_god_ray_trigger_body_entered(body):
 	if body.name == "player":
 		$OmniLight/GodRays.visible = true
+		$AudioStreamPlayerInterpolated.change_music(preload("res://OST/Kalimba.mp3"), 1)
 
 
 func _on_god_ray_trigger_body_exited(body):
 	if body.name == "player":
 		$OmniLight/GodRays.visible = false
+		$AudioStreamPlayerInterpolated.change_music(preload("res://OST/Maid with the Flaxen Hair.mp3"), 1)
