c6858acbf7737adc4bde23db8c30eb39ed7e901a
night-glider <nightgliderdev@gmail.com>
2022-08-03 16:34:45 +0300

    reworked intro

--- locations/battle_transition.gd
+++ locations/battle_transition.gd
@@ -3,6 +3,12 @@ extends Control
 func _ready():
 	$AnimationPlayer.play("start")
 
+func switch_dialogue_box():
+	$DialogueLabel/Panel.visible = true
+	$DialogueLabel.anchor_left = 0.7
+	$DialogueLabel.anchor_right = 0.95
+	$DialogueLabel.anchor_top = 0.1
+	$DialogueLabel.anchor_bottom = 0.3
 
 func _on_AnimationPlayer_animation_finished(anim_name):
 	get_tree().change_scene("res://locations/battle/battle.tscn")
