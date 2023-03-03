ec334ad66cd93e06975155f689e136fe44d75fb5
night-glider <nightgliderdev@gmail.com>
2023-03-03 22:49:50 +0300

    fixed battle transition font issue + fixed font size in meist dialogue in battle

--- locations/battle_transition.gd
+++ locations/battle_transition.gd
@@ -2,6 +2,7 @@ extends Control
 
 func _ready():
 	$AnimationPlayer.play("start")
+	
 
 func switch_dialogue_box():
 	$DialogueLabel/NinePatchRect.visible = true
@@ -9,6 +10,7 @@ func switch_dialogue_box():
 	$DialogueLabel.anchor_right = 0.95
 	$DialogueLabel.anchor_top = 0.1
 	$DialogueLabel.anchor_bottom = 0.3
+	$DialogueLabel.remove_font_override("normal_font")
 
 func _on_AnimationPlayer_animation_finished(anim_name):
 	get_tree().change_scene("res://locations/battle/battle.tscn")
