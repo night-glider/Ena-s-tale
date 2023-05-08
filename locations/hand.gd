261ca730b7aa54fdc1e38420a4ceddda79e5d72f
night-glider <85761149+night-glider@users.noreply.github.com>
2023-05-08 20:59:56 +0300

    changed game over animation

--- locations/hand.gd
+++ locations/hand.gd
@@ -5,12 +5,15 @@ var stage = 0
 
 func init(soul_pos:Vector2):
 	destination = soul_pos
-	position = destination + Vector2(100, 0)
+	position = destination + Vector2(50, 0)
+	scale = Vector2.ONE * 0.75
 	modulate.a = 0
 
 func _process(delta):
 	if stage == 0:
 		position = lerp(position, destination, 0.025)
+		scale.x = lerp(scale.x, 1, 0.025)
+		scale.y = lerp(scale.y, 1, 0.025)
 		modulate.a = lerp(modulate.a, 1, 0.025)
 		if position.distance_to(destination) < 10:
 			stage = 1
