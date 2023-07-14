d0234e0719fd17305405c5f663280acd8c2a4759
night-glider <nightgliderdev@gmail.com>
2023-07-14 13:11:17 +0300

    small fix in 5 attack

--- objects/battle/attacks/attack5/bullet.gd
+++ objects/battle/attacks/attack5/bullet.gd
@@ -17,19 +17,23 @@ func init(spd:float, accel:float, bottom:int, scatt:float, scatt_count:int, smal
 	scatter_count = scatt_count
 	small_fireball_speed = small_spd
 
+func spawn_scatter():
+	for i in range(scatter_count):
+		var new_bullet = bullet_small.instance()
+		new_bullet.init(small_fireball_speed)
+		new_bullet.position.x = position.x + rand_range(-scatter, scatter)
+		new_bullet.position.y = position.y + rand_range(-scatter, scatter)
+		get_parent().add_child(new_bullet)
+
 func touch_player():
+	spawn_scatter()
 	queue_free()
 
 func _process(delta):
 	position.y += velocity
 	velocity+=acceleration
 	if position.y >= bottom_y:
-		for i in range(scatter_count):
-			var new_bullet = bullet_small.instance()
-			new_bullet.init(small_fireball_speed)
-			new_bullet.global_position.x = position.x + rand_range(-scatter, scatter)
-			new_bullet.global_position.y = position.y + rand_range(-scatter, scatter)
-			get_parent().add_child(new_bullet)
+		spawn_scatter()
 		queue_free()
 
 func _on_Timer_timeout():
